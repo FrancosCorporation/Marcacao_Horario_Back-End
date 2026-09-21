093177fcf262d951d6728234cbe59017f5e3d878
FrancoCorporation <rodolfofrancoxico@gmail.com>
2026-09-20 22:56:51 -0300

    fix: porta e MONGO_URI via env, erro amigavel de porta em uso e conexao mongo tratada

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://admin:pass@mongo:27017/', {useMongoClient: true})
+const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/marcacao'
+mongoose.connect(uri)
 mongoose.Promise=global.Promise
 module.exports= mongoose;
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB indisponivel:', err.message, '- defina MONGO_URI e suba o MongoDB.')
+})
--- src/start.js
+++ src/start.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 require('./controller/authController')(app)
 require('./controller/indexController')(app)
 
-app.listen(port,() => {
-  console.log("Listeing in http://192.168.1.106:"+port)
+const server = app.listen(port, () => {
+  console.log("Listening on http://localhost:" + port)
+})
+server.on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Porta ${port} ja esta em uso. Defina PORT=<outra porta> e tente novamente.`)
+    process.exit(1)
+  }
+  throw err
 })
